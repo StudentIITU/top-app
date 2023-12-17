f796cb390ee5ae199064592efd7ea45085a75810
nurbolatkoshkimbayev <31545@iitu.edu.kz>
2023-12-17 20:47:59 +0600

    First steps

---
+++ components/Button/Button.tsx
@@ -0,0 +1,18 @@
+import styles from './Button.module.css';
+import {ButtonProps} from "@/components/Button/Button.props";
+import cn from 'classnames';
+
+
+export const Button = ({ appearance, children }: ButtonProps): JSX.Element => {
+    return (
+        <button className={cn(styles.button, {
+            [styles.primary]: appearance == 'primary',
+            [styles.ghost]: appearance == 'ghost',
+
+        })}
+
+        >
+            {children}
+        </button>
+    )
+};
